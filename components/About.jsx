f21a420bcd5c4dd7fbeabbec1f86ed927a2d9122
DannyKoll <kolardaniel8532@gmail.com>
2026-05-21 15:18:53 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -22,7 +22,7 @@ export default function About() {
         <div className="grid lg:grid-cols-12 gap-10 lg:gap-16 items-center">
           {/* Image left */}
           <div className="order-2 lg:order-1 lg:col-span-5 reveal">
-            <div className="relative w-[88%] max-w-[20rem] sm:w-full sm:max-w-[21rem] lg:max-w-[24rem] mx-auto lg:mx-0">
+            <div className="relative w-[260px] max-w-[calc(100%-3rem)] sm:w-full sm:max-w-[21rem] lg:max-w-[24rem] mx-auto lg:mx-0">
               {/* Decorative offset border */}
               <div className="absolute -inset-2 sm:-inset-4 border border-gold-500/30 rounded-[1.8rem] translate-x-2 translate-y-2 sm:translate-x-3 sm:translate-y-3" />
               <div className="absolute -top-3 -left-3 sm:-top-6 sm:-left-6 w-14 h-14 sm:w-24 sm:h-24 border-l-2 border-t-2 border-gold-500/40 rounded-tl-2xl sm:rounded-tl-3xl" />
