f0e5414bc5b806092e78053f707889805b564515
DannyKoll <kolardaniel8532@gmail.com>
2026-06-19 15:52:19 +0200

    Add files via upload

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+const baseUrl = "https://danielkolar.cz";
+
+const routes = [
+  "/",
+  "/investice",
+  "/pravni-informace",
+  "/zpracovani-osobnich-udaju",
+  "/cookies",
+];
+
+export default function sitemap() {
+  const lastModified = new Date();
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified,
+    changeFrequency: route === "/" ? "weekly" : "monthly",
+    priority: route === "/" ? 1 : 0.7,
+  }));
+}
