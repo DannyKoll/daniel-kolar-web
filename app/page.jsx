5b9b9afb6a903537dbb5f15a825531d3be35dbb1
DannyKoll <kolardaniel8532@gmail.com>
2026-06-24 11:12:56 +0200

    Add finance as whole homepage section

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Services from "@/components/Services";
+import FinanceTogether from "@/components/FinanceTogether";
 import Process from "@/components/Process";
 import Testimonials from "@/components/Testimonials";
 import CTA from "@/components/CTA";
@@ -22,6 +23,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <FinanceTogether />
       <Process />
       <Testimonials />
       <CTA />
